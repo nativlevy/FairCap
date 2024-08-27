1eb9280418fbdfb73d57f7ffbd7f56ffe449ebfa
Benton Li <cl2597@cornell.edu>
2024-08-27 18:13:55 +0100

    Create logging_util.py

---
+++ logging_util.py
@@ -0,0 +1,22 @@
+import logging
+import os
+import sys
+from macro_config import ALGO_OUTPUT_PATH
+
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# <= WARNING: stdout
+# > WARNING: stderr
+logging.basicConfig(level=logging.DEBUG)
+
+
+def init_logger(model_name):
+    logger = logging.getLogger(model_name)
+    logger.setLevel(logging.DEBUG)
+    out_handler = logging.FileHandler(os.path.join(
+        os.getenv('HOME'), 'output', 'stdout.log'))
+    out_handler.setLevel(logging.DEBUG)
+    out_handler.setFormatter(formatter)
+    logger.addHandler(out_handler)
+    return logger
