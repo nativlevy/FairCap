49afdbafc3cf2beae566a7244ca62b3189e5b91e
Benton Li <cl2597@cornell.edu>
2024-09-30 18:16:18 +0100

    Add pruning option

--- src/tools/load_data.py
+++ src/tools/load_data.py
@@ -8,7 +8,7 @@ from typing import Any, Tuple
 import pandas as pd
 import pygraphviz as pgv
 
-def load_data(datatable_path: str, dag_path:str) -> Tuple[pd.DataFrame, Any]:
+def load_data(datatable_path: str, dag_path:str, prune: bool = False) -> Tuple[pd.DataFrame, Any]:
     """
     Load data from a CSV file into a pandas DataFrame.
 
@@ -27,6 +27,9 @@ def load_data(datatable_path: str, dag_path:str) -> Tuple[pd.DataFrame, Any]:
     logging.info(f"Loaded {len(df)} rows and {len(df.columns)} columns")
     DAG = pgv.AGraph(dag_path, directed=True)
     DAG_str = DAG.to_string().replace(DAG.get_name(), " ")
+    # When pruning is switched on, discard attributes that is not in the causal graph
+    if prune:
+        df = df.drop(set(df.columns).difference(DAG.nodes()), axis=1)
     return df, DAG_str
 
 
