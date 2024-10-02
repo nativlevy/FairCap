6211361bede1c03255f0deb96c9eaa5b45e39e38
Benton Li <cl2597@cornell.edu>
2024-10-02 01:21:24 +0100

    Update launch missile

--- miscellaneous/launch.sh
+++ miscellaneous/launch.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 ssh -o StrictHostKeyChecking=no -o ControlMaster=auto -o ControlPersist=2m -o ControlPath=~/.ssh/cm-%r@%h:%p bcyl2@node0.remote.fair-prescrip-pg0.utah.cloudlab.us git clone https://github.com/bentondecusin/FairPrescriptionRules 
+
+ssh -o StrictHostKeyChecking=no -o ControlMaster=auto -o ControlPersist=2m -o ControlPath=~/.ssh/cm-%r@%h:%p bcyl2@node0.remote.fair-prescrip-pg0.utah.cloudlab.us git config --global user.name bcyl2
+
+ssh -o StrictHostKeyChecking=no -o ControlMaster=auto -o ControlPersist=2m -o ControlPath=~/.ssh/cm-%r@%h:%p bcyl2@node0.remote.fair-prescrip-pg0.utah.cloudlab.us git config --global user.email cl2597@cornell.edu
+
+ssh -o StrictHostKeyChecking=no -o ControlMaster=auto -o ControlPersist=2m -o ControlPath=~/.ssh/cm-%r@%h:%p bcyl2@node0.remote.fair-prescrip-pg0.utah.cloudlab.us sudo apt-get install htop
