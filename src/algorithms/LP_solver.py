dfa2207b26e9c4d663a982415dc102b73749eeff
Benton Li <cl2597@cornell.edu>
2024-10-04 02:10:17 +0100

    Use Pseudo boolean

--- src/algorithms/LP_solver.py
+++ src/algorithms/LP_solver.py
@@ -58,6 +58,10 @@ def LP_solver(rxCandidates, idx_all, idx_protected, cvrg_constr, fair_constr, l1
     Returns:
         list: A list of selected set names that satisfy the constraints and maximize the objective.
     """
+    # TODO add soft constraint
+    # Sort Rx candidates so that low utility rules are selected first.
+    # Assuming that each individual always choose the worse treatment
+    rxCandidates.sort(reverse=True, key=lambda rx: rx.utility)
     solver = Optimize()
     m = len(idx_all)
     l = len(rxCandidates)
@@ -77,25 +81,24 @@ def LP_solver(rxCandidates, idx_all, idx_protected, cvrg_constr, fair_constr, l1
     
     # Constraint 1;
     # For all i, j: t[i][j] <= g[j] 
-    # Equivalent to 
+    # Equivalent to t[i][j] -> i in rx[j].covered and g[j]  
     for i in range(m):
         solver.add([Implies(t[i][j], i in rxCandidates[j].covered_idx and g[j]) for j in range(l)])
 
     # Constraint 2a;
     # For all j: g[j] <= Sum t[i][j]
     # Equivalent to g[j] => OR(t[i]) 
-    solver.add([Implies(g[j], Or([i in rxCandidates[j].covered_idx and t[i][j] for i in range(m)])) for j in range(l)]) 
+    solver.add([Implies(g[j], PbGe([(t[i][j], 1) for i in range(m)], 1)) for j in range(l)])     
     # Constraint 2b;
     # For all i: sum (t[i][j]) <= 1
-    solver.add([Sum([t[i][j] for j in range(l)]) <= 1 for i in range(m)])  
-
+    solver.add([PbLe([(tij, 1) for tij in t[i]], 1) for i in range(m)])  
     # Constraint 3;
     # Group coverage (if any)
     if cvrg_constr != None and 'group' in cvrg_constr['variant']:
         threshold = cvrg_constr['threshold'] 
         threshold_p = cvrg_constr['threshold_p'] 
-        solver.add(Sum([Or([t[i][j] for j in range(l)]) for i in range(m)]) > threshold * m)
-        solver.add(Sum([Or([t[i][j] for j in range(l)]) for i in idx_protected]) > threshold_p * mp)
+        solver.add(PbGe([(Or([t[i][j] for j in range(l)]), 1) for i in idx_all], int(threshold_p * mp)))
+        solver.add(PbGe([(Or([t[i][j] for j in range(l)]), 1) for i in idx_protected], int(threshold_p * mp)))
 
     # Constraint 4;
     # Group fairness (if any)
@@ -108,40 +111,12 @@ def LP_solver(rxCandidates, idx_all, idx_protected, cvrg_constr, fair_constr, l1
             ttl_util_u = Sum([Sum([If(t[i][j], w[j], 0) for i in idx_unprotected]) for j in range(l)])
             solver.add(Abs(ttl_util_p * num_u  - ttl_util_u * num_p)  < threshold * num_p * num_u)
             
-    with StopWatch("Solving"):
-        # Check for satisfiability and retrieve the optimal solution
-        if solver.check() == sat:
-            model = solver.model()
-            selected_sets = [rxCandidates[j] for j in range(l) if is_true(model[g[j]])]
-            print(len(selected_sets))
-            return selected_sets
-        else:
-            logging.warning("No solution was found!")
-            return []
-    
-
-
-def main():
-    with open("/Users/bcyl/FairPrescriptionRules/output/10-01/20:36/greedy/rules_greedy_all.json") as f:
-        data = json.load(f)
-    rxCandidates = []
-    for rule in data:
-        rxCandidates.append(Prescription(rule['condition'], rule['treatment'], set(rule['coverage']), set(rule['protected_coverage']), rule['utility'], rule['protected_utility']))
-    df = pd.read_csv("/Users/bcyl/FairPrescriptionRules/data/stackoverflow/so_countries_col_new_mini.csv")
-    df = df.drop(['Unnamed: 0'], axis=1, errors='ignore')
-    idx_protected = set(df[df['RaceEthnicity'] != 'White or of European descent'].index)
-    l = 10
-    m = 100
-    set_param('sat.lookahead_simplify', True) 
-    set_param("parallel.enable", True)
-    set_param('parallel.threads.max', 10000)
-    
-    cvrg_constr = {
-        'variant': 'group',
-        'threshold': 0.8,
-        'threshold_p': 0.8,
-    }
-    with StopWatch(""):
-        LP_solver(rxCandidates, set(df.index), idx_protected, cvrg_constr, None)
-    return
-
+    # Check for satisfiability and retrieve the optimal solution
+    if solver.check() == sat:
+        model = solver.model()
+        selected_sets = [rxCandidates[j] for j in range(l) if is_true(model[g[j]])]
+        print(len(selected_sets))
+        return selected_sets
+    else:
+        logging.warning("No solution was found!")
+        return []
